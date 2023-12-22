339058fc3f2f9cf7fd5272e72b3c510bfdb20284
MilanStalwart <milan.stalwartdigital@gmail.com>
2023-12-22 10:14:39 +0530

    Changed ngrok link

--- demo/config.js
+++ demo/config.js
@@ -1,6 +1,6 @@
-// https://cb63-2401-4900-1c80-28ee-49ba-73a1-3c63-dd90.ngrok-free.app/auth/callback
-// https://cb63-2401-4900-1c80-28ee-49ba-73a1-3c63-dd90.ngrok-free.app/auth/shopify/callback
-// https://cb63-2401-4900-1c80-28ee-49ba-73a1-3c63-dd90.ngrok-free.app/api/auth/callback
+// https://2bd5-2401-4900-1c80-28ee-8411-8f92-ad2d-62ab.ngrok-free.app/auth/callback
+// https://2bd5-2401-4900-1c80-28ee-8411-8f92-ad2d-62ab.ngrok-free.app/auth/shopify/callback
+// https://2bd5-2401-4900-1c80-28ee-8411-8f92-ad2d-62ab.ngrok-free.app/api/auth/callback
 
 import express from "express";
 const app = express();
@@ -11,7 +11,7 @@ import { restResources } from "@shopify/shopify-api/rest/admin/2023-10";
 const shopify = shopifyApi({
   apiKey: "564787232f953504bf617e4975ff5fc6",
   apiSecretKey: "53076a6fbaa0eaf81e75f738eb0a557f",
-  hostName: "cb63-2401-4900-1c80-28ee-49ba-73a1-3c63-dd90.ngrok-free.app/",
+  hostName: "2bd5-2401-4900-1c80-28ee-8411-8f92-ad2d-62ab.ngrok-free.app",
   adminApiAccessToken: "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",
   isCustomStoreApp: true,
   restResources,
